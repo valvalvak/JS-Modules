3a24e709177eb39541f1f80d7e410e729de779ed
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-17 23:15:10 +0300

    update

--- 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/04. Previous Day.js
+++ 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/04. Previous Day.js
@@ -1,7 +1,19 @@
 function solve(year, month, day) {
-    let theDayBefore = new Date(date.getTime())
-    theDayBefore.setDate(date.getDate() - 1);
-    console.log(theDayBefore);
+    if (day)
+    const date = Temporal.PlainDate.from({ year: year, month: month, day: day }); // => 2006-08-24
+    date.year; // => 2006
+    date.inLeapYear; // => false
+    date.toString(); // => '2006-08-24'
 }
 solve(2016, 9, 30);
 solve(2016, 10, 1);
+
+// size = matrix.length - 1
+// currentRow = x // from [x, y]
+// currentCol = y // from [x, y]
+// for (i = 0; i >= matrix.length; i++){
+//     matrix[i][i] // @first diagonal
+//     matrix[size - i][size - i] // @second diagonal
+//     matrix[currentRow][i] // @row check
+//     matrix[i][currentCol] // @col check
+// }

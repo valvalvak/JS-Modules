f410285ee520a8717b6613893f29ee3e8dfbefe1
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-19 16:19:33 +0300

    update

--- 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/05. Time to Walk.js
+++ 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/05. Time to Walk.js
@@ -1,7 +1,21 @@
-function solve(steps, footprint, kmph){
-    let distance = steps * footprint
-
+function solve(steps, footprint, kmph) {
+    function formatNumber(number) {
+        if (number < 10) {
+            return '0' + number.toString()
+        } else {
+            return number
+        }
+    }
+    let distance = steps * footprint;
+    let mph = kmph * 1000;
+    let minutesRestBreak = Math.floor(distance / 500) / 60;
+    let travelTime = (distance / mph) + minutesRestBreak;
+    let hours = Math.floor(travelTime)
+    let minutes = Math.floor(travelTime * 60);
+    let seconds = Math.round(((travelTime * 60) - minutes) * 60);
+    console.log(`${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`);
 
 }
-solve();
-solve();
+// solve(4000, 0.60, 5);
+// solve(2564, 0.70, 5.5);
+

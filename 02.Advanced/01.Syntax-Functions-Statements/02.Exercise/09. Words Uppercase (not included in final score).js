e904436082ba605d50387a77d21849a3952cbf7f
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-22 01:31:09 +0300

    update

--- 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/09. Words Uppercase (not included in final score).js
+++ 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/09. Words Uppercase (not included in final score).js
@@ -1,4 +1,9 @@
-function solve(){
+function solve(givenString){
+    let strArr = [];
+    let re = /[A-Za-z]+/gm
+    strArr = [...givenString.matchAll(re)]
+    console.log(strArr.join(", ").toUpperCase())
 
 }
-solve()
+// // solve("Hi, how are you?")
+// solve('hello')

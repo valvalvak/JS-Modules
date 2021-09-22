15e97b81fc407808a048515d330bf2237c71f2c5
valvalvak <vvakrilov@gmail.com>
2021-09-22 13:29:49 +0300

    update

--- 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/09. Words Uppercase (not included in final score).js
+++ 02.Advanced/01.Syntax-Functions-Statements/02.Exercise/09. Words Uppercase (not included in final score).js
@@ -3,7 +3,6 @@ function solve(givenString){
     let re = /[A-Za-z]+/gm;
     strArr = [...givenString.matchAll(re)];
     console.log(strArr.join(", ").toUpperCase());
-
 }
 solve("Hi, how are you?");
 solve('hello');

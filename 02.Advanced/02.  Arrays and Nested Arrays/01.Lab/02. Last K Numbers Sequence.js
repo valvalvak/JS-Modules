31ad95fd91be342a7a5c300ede85d5610a791af7
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-25 01:01:21 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/01.Lab/02. Last K Numbers Sequence.js
@@ -0,0 +1,18 @@
+function solve(n, k){
+    const arr = [1]
+    arr.fill(0, 1, n);
+    for(let i = 1; i < n; i++){
+        if (i < k){
+            sum = arr.reduce((a,b) => a+b);
+        }else{
+            let index = Number(arr.length - k);
+            let newArr = arr.slice(index);
+            sum = newArr.reduce((a,b) => a+b);
+        }
+        arr.push(sum)
+
+    }
+    return arr
+}
+// console.log(solve(6, 3));
+// console.log(solve(8, 2));

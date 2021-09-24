4d43eef38ec79cd9906293f9514a11c98d05d11b
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-25 02:24:48 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/01.Lab/05. Smallest Two Numbers.js
@@ -0,0 +1,8 @@
+function solve(array){
+    let resultArray = [...array];
+    resultArray.sort((a, b) => a-b);
+    resultArray.splice(2, resultArray.length)
+    console.log(resultArray.join(" "));
+}
+// solve([30, 15, 50, 5]);
+// solve([3, 0, 10, 4, 7, 3]);

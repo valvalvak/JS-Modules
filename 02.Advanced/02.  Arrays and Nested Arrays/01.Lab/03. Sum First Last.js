c211b79bcab5ff8cf6e07256ff19f22e7843af60
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-25 01:02:01 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/01.Lab/03. Sum First Last.js
@@ -0,0 +1,9 @@
+function solve(array){
+    let a = Number(array[0]);
+    let b = Number(array[array.length-1]);
+    let res = a+b;
+    return res
+
+}
+// console.log(solve(["20","30","40"]));
+// console.log(solve(["5","10"]));

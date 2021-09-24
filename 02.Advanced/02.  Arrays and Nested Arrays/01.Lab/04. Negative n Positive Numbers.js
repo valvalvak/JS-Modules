9a3d95a3f98a77a91665206ceb8677471cda91af
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-25 01:39:07 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/01.Lab/04. Negative n Positive Numbers.js
@@ -0,0 +1,13 @@
+function solve(givenArray) {
+    let resultArray = [];
+    for (i = 0; i < givenArray.length; i++) {
+        if (givenArray[i] < 0) {
+            resultArray.unshift(givenArray[i]);
+        }else{
+            resultArray.push(givenArray[i]);
+        }
+    }
+    resultArray.forEach(element =>  console.log(element));
+}
+// solve([7, -2, 8, 9]);
+// solve([3, -2, 0, -1]);

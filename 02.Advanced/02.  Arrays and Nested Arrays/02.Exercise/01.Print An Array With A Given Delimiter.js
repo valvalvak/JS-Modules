bb210073c34f0c95b40ed312658add08d1d59f42
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-25 18:08:35 +0300

    update

--- 02.Advanced/02.  Arrays and Nested Arrays/02.Exercise/01.Print An Array With A Given Delimiter.js
+++ 02.Advanced/02.  Arrays and Nested Arrays/02.Exercise/01.Print An Array With A Given Delimiter.js
@@ -1,4 +1,18 @@
-function solve(){
-
+function solve(...arr) {    
+    let separator = arr[1].toString()
+    console.log(arr[0].join(separator));
 }
-solve();
+
+// solve(['One',
+//     'Two',
+//     'Three',
+//     'Four',
+//     'Five'],
+//     '-');
+// solve(['How about no?',
+//     'I',
+//     'will',
+//     'not',
+//     'do',
+//     'it!'],
+//     '_');

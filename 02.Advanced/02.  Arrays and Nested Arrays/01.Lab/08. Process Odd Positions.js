e79d59db5304e3fef138b5a96f6f5199fd8fb720
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-25 03:14:05 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/01.Lab/08. Process Odd Positions.js
@@ -0,0 +1,11 @@
+function solve(mixedArray){
+    let result = [];
+    for (let i = 1; i < mixedArray.length; i+=2){
+        let value = mixedArray[i] * 2;
+        result.unshift(value);            
+        }
+    return result.join(" ");
+
+}
+// console.log(solve([10, 15, 20, 25]));
+// console.log(solve([3, 0, 10, 4, 7, 3]));

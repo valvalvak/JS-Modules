a1f957003f6cb52fee4ce5025ba3552317c1720a
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-26 13:49:41 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/02.Exercise/06.List Of Names.js
@@ -0,0 +1,8 @@
+function solve(array){
+    let result = [];
+    array.sort();
+    for (let i = 0; i < array.length; i++){
+        console.log(`${i+1}.${array[i]}`);
+    }
+}
+// solve(["John", "Bob", "Christina", "Ema"]);

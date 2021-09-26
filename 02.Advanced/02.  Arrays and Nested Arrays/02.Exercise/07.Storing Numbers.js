2439626fb566486dadfa37a649150e46437bc738
Valentin Vakrilov <vvakrilov@gmail.com>
2021-09-26 15:16:05 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/02.Exercise/07.Storing Numbers.js
@@ -0,0 +1,15 @@
+function solve(array){
+    let sortedArray = [];
+    let result = [];
+    sortedArray = array.sort((a, b,) => a-b);
+    while (sortedArray.length > 0){
+        result.push(sortedArray.shift());
+        result.push(sortedArray.pop());
+    }
+    return result;
+    // console.log(sortedArray);
+    // console.log(result);
+}
+    
+
+// console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));

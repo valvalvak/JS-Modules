d8762809ac40e1721184d5616ccf90d3352e76ab
valvalvak <vvakrilov@gmail.com>
2021-09-27 18:04:08 +0300

    update

---
+++ 02.Advanced/02.  Arrays and Nested Arrays/01.Lab/04. Negative n Positive Numbers.js
@@ -0,0 +1,8 @@
+// function solve(arr){
+//     let newArr = arr.sort((a, b) => a-b);
+//     newArr.forEach(element => {
+//         console.log(element);
+//     });
+// }
+// solve([7, -2, 8, 9]);
+// solve([3, -2, 0, -1]);
--- 02.Advanced/useful.js
+++ 02.Advanced/useful.js
@@ -1,8 +1,8 @@
-
 // const compareNumbers = (a, b) => a - b;
 // const compareLetters = (a, b) => a.localeCompare(b)
+/*
 
-
+*/
 // function customSlice(customArray) {
 //     let newArr = new Array();
 //     for (let i = 0; i < customArray.length; i++) {
@@ -14,3 +14,15 @@
 // arr = customSlice(['| aaa | bbb | ccc |',
 //     '| ddd | eee | fff |'])
 // console.log(arr)
+
+/*
+
+*/
+// function solve(arr){
+//     let newArr = arr.sort((a, b) => a-b);
+//     newArr.forEach(element => {
+//         console.log(element);
+//     });
+// }
+// solve([7, -2, 8, 9]);
+// solve([3, -2, 0, -1]);

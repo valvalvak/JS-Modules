ff5e9f28f88561a04b53c274e633217f62d39a06
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-01 18:58:54 +0300

    update

---
+++ 02.Advanced/03.Objects and Composition/02.Exercise/01.Calorie Object.js
@@ -0,0 +1,12 @@
+function solve(array){
+    const result = {}
+    for (let i = 1; i< array.length; i+=2){
+        let product = array[i-1];
+        let value = Number(array[i]);
+        result[product] = value;
+    }
+    console.log(result)
+}
+
+// solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
+// solve(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']);

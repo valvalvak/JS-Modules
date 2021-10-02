7a7f85caa92e56c70ba29305de18f89f28a2148c
valvalvak <vvakrilov@gmail.com>
2021-10-02 16:36:16 +0300

    update

---
+++ 02.Advanced/03.Objects and Composition/02.Exercise/04.Heroic Inventory.js
@@ -0,0 +1,34 @@
+// function solve(arr){
+//     let res = [];
+//     for (let str of arr){
+//         let currentHero = str.split(" / ")
+//         let hero = {
+//         name: currentHero[0],
+//         level: Number(currentHero[1]),
+//         items: currentHero.slice(2)
+//         };
+//     res.push(hero);
+//     }
+// }
+
+function solve(arr){
+    let solutionResult = [];
+    for (const str of arr){
+        let [name, level, items] = str.split(" / ");
+        level = Number(level);
+        items = items ? items.split(", ") : [];
+        let currentHero = {name, level, items}
+        solutionResult.push(currentHero)
+    }
+    console.log(JSON.stringify(solutionResult));
+}
+
+// solve([
+//     'Isaac / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antra'
+// ]);
+
+// solve(
+//     ['Jake / 1000 / Gauss, HolidayGrenade']
+// );

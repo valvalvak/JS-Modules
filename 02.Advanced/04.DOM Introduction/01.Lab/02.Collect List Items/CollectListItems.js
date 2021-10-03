a2afaf61563d5bf3e649393cc33454f03fb14abd
valvalvak <vvakrilov@gmail.com>
2021-10-04 00:03:24 +0300

    update

--- 02.Advanced/04.DOM Introduction/01.Lab/02.Collect List Items/CollectListItems.js
+++ 02.Advanced/04.DOM Introduction/01.Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    let itemsNodes = document.querySelectorAll("ul#items li");
+    let textarea = document.querySelector("#result");
+    for(let node of itemsNodes) {
+        textarea.value += node.textContent + "\n";
+    }
 }

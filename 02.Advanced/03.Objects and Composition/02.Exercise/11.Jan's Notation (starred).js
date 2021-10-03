1e647a2dbb74d8a8ff59b1a3ccede94f09bb2d83
valvalvak <vvakrilov@gmail.com>
2021-10-03 20:08:10 +0300

    update

--- 02.Advanced/03.Objects and Composition/02.Exercise/11.Jan's Notation (starred).js
+++ 02.Advanced/03.Objects and Composition/02.Exercise/11.Jan's Notation (starred).js
@@ -1,24 +1,38 @@
 function solve(array){
     const operands = [];
     let result = Number;
-    array.forEach(element => {
+    for (let element of array){
         if (typeof element === "number"){
-            operands.unshift(Number(element));
+            operands.push(Number(element));
         }else{
-            let a = operands.shift();
-            let b = operands.shift();
-            if (element == "+"){
-                result = a + b;
-            }else if (element == "-"){
-                result = a - b;
-            }else if (element == "*"){
-                result = a * b;
-            }else if (element == "/"){
-                result = a / b;
-            }
+            if (operands.length >= 2){
+                let b = operands.pop();
+                let a = operands.pop();
+                if (element == "+"){
+                    result = a + b;
+                    operands.push(result);
+                }else if (element == "-"){
+                    result = a - b;
+                    operands.push(result);
+                }else if (element == "*"){
+                    result = a * b;
+                    operands.push(result);
+                }else if (element == "/"){
+                    result = a / b;
+                    operands.push(result);
+                }
+            }else{
+                return console.log("Error: not enough operands!");
             }
         }
-    );
-    console.log(result)
+    }
+    if (operands.length > 1){
+        console.log("Error: too many operands!");
+    }else{
+        console.log(result)
+    }
 }
-solve([3, 4, '+'])
+// solve([3, 4, '+']);
+// solve([5, 3, 4, '*', '-']);
+// solve([7, 33, 8, '-'])
+// solve([15, '/'])

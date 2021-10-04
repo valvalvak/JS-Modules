30a153cf3c6754f67bab955402d802fa7f613ab1
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-05 02:05:26 +0300

    update

--- 02.Advanced/04.DOM Introduction/01.Lab/03.Sum Numbers/SumNumbers.js
+++ 02.Advanced/04.DOM Introduction/01.Lab/03.Sum Numbers/SumNumbers.js
@@ -1,7 +1,11 @@
-// function calc() {
-//     const num1 = document.getElementById('num1').value;
-//     const num2 = document.getElementById('num2').value;
-//     const result = Number(num1) + Number(num2);
-
-//     document.getElementById('sum').value = result;
-// }
+function calc() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const result = Number(num1) + Number(num2);
+    // if(Number.isNaN(result)) {
+    //     document.getElementById('sum').value = "Please enter a number!";
+    // } else {
+    //     document.getElementById('sum').value = result;
+    // }
+    document.getElementById('sum').value = result;
+}

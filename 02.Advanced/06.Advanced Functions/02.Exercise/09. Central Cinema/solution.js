d2b4e16ab5777fdbacb23396f4819d739d81218e
valvalvak <vvakrilov@gmail.com>
2021-10-04 20:09:56 +0300

    update

--- 02.Advanced/06.Advanced Functions/02.Exercise/09. Central Cinema/solution.js
+++ 02.Advanced/06.Advanced Functions/02.Exercise/09. Central Cinema/solution.js
@@ -157,3 +157,67 @@
 //         e.target.parentElement.remove();
 //     }
 // }
+
+
+/**
+ * function solve() {
+    const [name, hall, ticketPrice] = document.querySelectorAll("#container input");
+    const movieSection = document.querySelector("#movies ul");
+    const archiveSection = document.querySelector("#archive ul");
+    const clearButton = archiveSection.parentElement.querySelector("button");
+    clearButton.addEventListener("click", ()=> {
+        archiveSection.innerHTML = "";
+    })
+    const addMovieButton = document.querySelector("#container button");
+    addMovieButton.addEventListener("click", addMovie);
+
+
+
+    function addMovie(e) {
+        e.preventDefault();
+        if (name.value !== "" && hall.value !== "" && ticketPrice.value != "" && !isNaN(Number(ticketPrice.value))) {
+            const movie = document.createElement("li");
+            movie.innerHTML =
+                `<span>${name.value}</span>
+                 <strong>${hall.value}</strong>
+                <div>
+                    <strong>${Number(ticketPrice.value).toFixed(2)}</strong>
+                    <input placeholder="Tickets Sold">
+                    <button >Archive</button>
+                </div>`
+            movieSection.appendChild(movie);
+
+            const button = movie.querySelector("div button");
+            button.addEventListener("click", addToArchive);
+            name.value = "";
+            hall.value = "";
+            ticketPrice.value = "";
+        }
+    }
+
+
+    function addToArchive(e) {
+        const inputValue = e.target.parentElement.querySelector("input");
+        const ticketPrice = e.target.parentElement.querySelector("strong");
+        const movieName = e.target.parentElement.parentElement.querySelector("span");
+        if (inputValue.value != "" && !isNaN(Number(inputValue.value))) {
+            const income = Number(inputValue.value) * Number(ticketPrice.textContent);
+
+            const liEl = document.createElement("li");
+            liEl.innerHTML = `<span>${movieName.textContent}</span>
+                              <strong>Total amount: ${income.toFixed(2)}</strong>
+                              <button>Delete</button>`
+
+            const button = liEl.querySelector("button");
+            button.addEventListener("click", deleteEntry);
+            archiveSection.appendChild(liEl);
+        }
+
+        e.target.parentElement.parentElement.remove();
+    }
+
+    function deleteEntry(e) {
+        e.target.parentElement.remove();
+    }
+}
+ */

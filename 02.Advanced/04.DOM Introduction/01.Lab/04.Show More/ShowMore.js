8621fdcad4c09c8b2570fe0ad4ac375345c65ce8
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-05 20:11:40 +0300

    update

--- 02.Advanced/04.DOM Introduction/01.Lab/04.Show More/ShowMore.js
+++ 02.Advanced/04.DOM Introduction/01.Lab/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+    const text = document.getElementById('text');
+    text.style.display = 'inline';
+    const button = document.getElementById('more')
+    button.style.display = 'none';
 }

bd38f7e8fae64f61455c7b2da5f0840926ef123a
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-05 23:01:56 +0300

    update

--- 02.Advanced/04.DOM Introduction/01.Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 02.Advanced/04.DOM Introduction/01.Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,11 @@
 function extract(content) {
-
+    const text = document.getElementById(content).textContent;
+    const pattern = /\((.+?)\)/g;
+    let match = pattern.exec(text);
+    const result = []
+    while(match != null) {
+        result.push(match)
+        match = pattern.exec(text)
+    }
+    return result.join("; ")
 }

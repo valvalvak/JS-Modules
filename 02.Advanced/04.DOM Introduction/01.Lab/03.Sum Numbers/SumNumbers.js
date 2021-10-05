57df9af49dae53320d409e3ce09550bb6f3e28f6
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-05 20:12:54 +0300

    update

--- 02.Advanced/04.DOM Introduction/01.Lab/03.Sum Numbers/SumNumbers.js
+++ 02.Advanced/04.DOM Introduction/01.Lab/03.Sum Numbers/SumNumbers.js
@@ -2,6 +2,11 @@ function calc() {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
     const result = Number(num1) + Number(num2);
+    
+    /**
+     * if control needed uncomment if following statement:
+     */
+
     // if(Number.isNaN(result)) {
     //     document.getElementById('sum').value = "Please enter a number!";
     // } else {

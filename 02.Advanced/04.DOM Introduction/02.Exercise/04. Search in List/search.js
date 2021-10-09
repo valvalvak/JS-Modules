2c48edb9941a7f8ca707f7232a246d920cfb7861
valvalvak <vvakrilov@gmail.com>
2021-10-10 00:26:19 +0300

    update

--- 02.Advanced/04.DOM Introduction/02.Exercise/04. Search in List/search.js
+++ 02.Advanced/04.DOM Introduction/02.Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   // const cityArray = document.getElementById("towns").ariaValueMax;
+   const citiesArray = Array.from(document.querySelectorAll("ul li"));
+   const text = document.getElementById("searchText").value;
+   let matchesFound = 0;
+   citiesArray.forEach(element => {
+      if(element.innerHTML.includes(text)) {
+         element.style.fontWeight = "bold";
+         element.style.textDecoration = "underline";
+         matchesFound++;
+      } else {
+         element.style.fontWeight = "normal";
+         element.style.textDecoration = "";
+      }
+   })
+   document.getElementById("result").textContent = `${matchesFound} matches found`
 }

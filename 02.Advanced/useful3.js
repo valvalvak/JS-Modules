b3f4b0517d3409e66276a4bf2540a377c4ec10ab
valvalvak <vvakrilov@gmail.com>
2021-10-09 21:47:13 +0300

    update

--- 02.Advanced/useful3.js
+++ 02.Advanced/useful3.js
@@ -1,12 +1,12 @@
-function thisOne(){
-    let a = 5; //a == 5
-    console.log(a)
-}
+// function thisOne(){
+//     let a = 5; //a == 5
+//     console.log(a)
+// }
 
-function thisTwo(){
-    let a = 10;
-    console.log(a) //a == 10
-}
+// function thisTwo(){
+//     let a = 10;
+//     console.log(a) //a == 10
+// }
 
-thisOne();
-thisTwo();
+// thisOne();
+// thisTwo();

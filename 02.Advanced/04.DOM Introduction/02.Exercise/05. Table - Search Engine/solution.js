49e517ce4b6f74086908477af2181d4f9f494a78
valvalvak <vvakrilov@gmail.com>
2021-10-10 01:04:58 +0300

    update

--- 02.Advanced/04.DOM Introduction/02.Exercise/05. Table - Search Engine/solution.js
+++ 02.Advanced/04.DOM Introduction/02.Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   
+   function onClick() {      
+      const searchString = document.getElementById("searchField");
+      let tableElements = Array.from(document.querySelectorAll("tbody tr"));
+      let textValue = searchString.value.toLowerCase(); 
 
-   function onClick() {
-      //   TODO:
-
+      tableElements.forEach(element => {
+         let currentString = element.textContent.toLowerCase();
+         if(currentString.includes(textValue)) {
+            element.classList.add("select");
+         } else {
+            element.classList.remove("select")
+         }
+      });
+      searchString.value = "";
    }
 }

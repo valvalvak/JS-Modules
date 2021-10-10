74b3340624c7782b04683e102f7eaf11b2ea9aa0
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-10 23:18:16 +0300

    update(undone)

--- 02.Advanced/04.DOM Introduction/02.Exercise/07. Hell's Kitchen/app.js
+++ 02.Advanced/04.DOM Introduction/02.Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,31 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+   document.querySelector('#btnSend').addEventListener('click',onClick);
+   const restaurantDataLog = {}
 
-   function onClick () {
-      //   TODO:
-      
+   function splitInputArray(array) {
+      array.forEach(restData => {
+         let currentData = restData.split(" - ");
+         [restaurant,staffArray] = currentData;
+         if(!restaurant in restaurantDataLog) {
+            
+         }
+
+         let staff = {
+            worker,
+            salary,
+         };
+         staffArray.forEach(worker => {
+            let [w,s] = worker;
+            staff.worker = w;
+            staff.salary = s;
+         });
+
+      });
    }
-}
+   
+
+   function onClick() {
+
+   };
+};
+   

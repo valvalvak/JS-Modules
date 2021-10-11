a83328f4d8866595ebcc44da3c93ac67c1f1e304
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-11 23:39:18 +0300

    update(undone)

--- 02.Advanced/04.DOM Introduction/02.Exercise/07. Hell's Kitchen/app.js
+++ 02.Advanced/04.DOM Introduction/02.Exercise/07. Hell's Kitchen/app.js
@@ -1,31 +1,38 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click',onClick);
-   const restaurantDataLog = {}
+   document.querySelector('#btnSend').addEventListener('click', onClick);
+   const restaurantDataLog = {
+      average() {
+         Math.sum(this.salaryArray) / this.salaryArray.length
+      },
+      bestWorkerSalary() {
+         Object.entries(this.staff).filter((a, z) => { z - a })
+      }
+   }
 
-   function splitInputArray(array) {
+   function solution(array) {
       array.forEach(restData => {
          let currentData = restData.split(" - ");
-         [restaurant,staffArray] = currentData;
-         if(!restaurant in restaurantDataLog) {
+         [restaurant, staffArray] = currentData;
+         if (!restaurantDataLog[restaurant]) {
+            restaurantDataLog[restaurant] = restaurant,
+               staff = {},
+               salaryArray = []
+         }
+         for (let worker of staffArray) {
+            [workerName, workerSalary] = worker
+            if (!restaurantDataLog[restaurant].staff[workerName]) {
+               restaurantDataLog[restaurant].staff[worker] = workerSalary,
+                  restaurantDataLog.salaryArray.push()
+            }
+         }
+      });
+      getBestRestaurantSalary(result => {
+         for (let mayTheBestWin in restaurantDataLog) {
             
          }
-
-         let staff = {
-            worker,
-            salary,
-         };
-         staffArray.forEach(worker => {
-            let [w,s] = worker;
-            staff.worker = w;
-            staff.salary = s;
-         });
-
-      });
+      })
    }
-   
-
    function onClick() {
 
    };
 };
-   

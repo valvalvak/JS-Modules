f7881fbd4241d11ed97fa35f98d4584815eb2fb1
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-17 18:57:04 +0300

    update

---
+++ 03.JS_Adv_exam_prep_1/Test/test.js
@@ -0,0 +1,16 @@
+const testNumbers = require('./testNumbers');
+const { expect } = require('chai');
+
+describe('Test Numbers', () => {
+    
+    describe('sumNumbers', () => {
+        
+    });
+    describe('numberChecker', () => {
+
+    });
+    describe('averageSumArray', () => {
+
+    });
+    
+});

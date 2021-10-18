b24df8a820f9315ef375a4d4bf0fd94213ef3701
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-18 22:24:30 +0300

    update

--- 03.JS_Adv_exam_prep_2/02_Restaurant/Restaurant.js
+++ 03.JS_Adv_exam_prep_2/02_Restaurant/Restaurant.js
@@ -1,71 +1,73 @@
 class Restaurant {
     constructor(budget) {
-        this.budgetMoney = budget;
+        this.budgetMoney = Number(budget);
         this.menu = {};
         this.stockProducts = {};
         this.history = [];
-    }
-
+        this._menuItemsCount = Object.keys(this.menu).length;
+    };
+    
     loadProducts(products) {
-        let stringResultLog = [];
 
-        products.forEach(element => {
-
-            [productName, productQuantity, productTotalPrice] = element.split(" ");
+        for (let el of products) {
+            let [productName, productQuantity, productTotalPrice] = el.split(" ");
+            productQuantity = Number(productQuantity);
+            productTotalPrice = Number(productTotalPrice);
 
             if (productTotalPrice <= this.budgetMoney) {
-
-                if (this.stockProducts.hasOwnProperty(productName)) {
-                    this.stockProducts[productName] += productQuantity;
-                } else {
-                    this.stockProducts[productName] = productQuantity;
-                };
-
+                if (!this.stockProducts[productName]) {
+                    this.stockProducts[productName] = 0;
+                }
+                this.stockProducts[productName] += productQuantity;
                 this.budgetMoney -= productTotalPrice;
-                stringResultLog.push(`Successfully loaded ${productQuantity} ${productName}`);
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+            }
+            else {this.history.push(`There was not enough money to load ${productQuantity} ${productName}`)}
+        };
+        return this.history.join('\n');
+    };
 
-            } else {
-                stringResultLog.push(`There was not enough money to load ${productQuantity} ${productName}`)
-            };
-
-            return stringResultLog.join('\n');
-        });
-
-
-    }
-
-    countObjProp(obj) {
-        let counter = 0;
-        for (let _ in obj) {
-            counter++;
-        }
-        return counter;
-    }
-
-    addToMenu(meal, neededProducts, price) {
-        [productName, productQuantity] = neededProducts.split(" ");
+    addToMenu(meal, products, price) {
         if (this.menu.hasOwnProperty(meal)) {
-            return `The ${meal} is already in the our menu, try something different.` // must be: "The meal is on the menu".
+            return `The ${meal} is already in the our menu, try something different.`; // must be: "The meal is on the menu".
         } else {
-            let mealsOnMenu = this.countObjProp(this.menu);
-            if (mealsOnMenu == 1) {
-                
+            this.menu[meal] = {
+                products: {},
+                price: price
+            };
+            
+            products.forEach(element => {
+                let [productName, productQuantity] = element.split(" ");
+                productQuantity = Number(productQuantity);
+                this.menu[meal].products[productName] = productQuantity;
+            });
+
+            if (this._menuItemsCount == 1) {
+                return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+            } else {
+                return `Great idea! Now with the ${meal} we have ${this._menuItemsCount} meals in the menu, other ideas?`;
             }
         }
+    };
 
-    }
     showTheMenu() {
+        if (this._menuItemsCount < 1) {
+            return "Our menu is not ready yet, please come later..."
+        }
+        let menu = [];
+        for (let meal in this.menu) {
+            menu.push(`${meal} - $ ${this.menu[meal]}`)
+        }
+        return menu.join('\n')
+    }
+    makeTheOrder(meal) {
 
     }
-    makeTheOrder() {
-
-    }
-
 }
 
 //input #1
-let kitchen = new Restaurant(1000);
-console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
 
 // Output #1
 // Successfully loaded 10 Banana
@@ -78,9 +80,9 @@ console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 5
 
 
 //input #2
-let kitchen = new Restaurant(1000);
-console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
-console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
+// console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
 
 // Output #2
 // Great idea! Now with the frozenYogurt we have 1 meal in the menu, other ideas?
@@ -88,8 +90,8 @@ console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Sa
 
 
 //input #3
-let kitchen = new Restaurant(1000);
-console.log(kitchen.showTheMenu());
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.showTheMenu());
 
 // Output #3
 // frozenYogurt - $ 9.99

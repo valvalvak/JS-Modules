a6104fd6bc46ada04bcb93ddf15c3d0f52344117
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-11-02 19:58:05 +0200

    update

--- 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/01.Bus-Stop/app.js
+++ 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopID = document.getElementById('stopID').ariaValueMax;
+    const busStopNameElement = document.getElementById('stopName');
+    const timeTableElement = document.getElementById(`buses`);
+
+    const url = `http://localhost:3030/jsonstore/bus/buinfo/${stopID}`;
+
+    try {
+        const res = await fetch(url);
+        if (res.status != 200) {
+            throw new Error('Stop ID not found');
+        }
+        const data = await res.json();
+
+        busStopNameElement.textContent = data.name;
+        Object.entries(data.buses).forEach((b, v) => {
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${b} arrives in ${v} minutes`;
+            timeTableElement.appendChild(liElement);
+        })
+    } catch (error) {
+        alert(error.message);   
+    }
 }

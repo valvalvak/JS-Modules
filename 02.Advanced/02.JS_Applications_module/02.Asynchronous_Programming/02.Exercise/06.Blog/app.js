7b0648edcf6141b446260b69066fc50c1547f443
Valentin Vakrilov <vvakrilov@gmail.com>
2021-11-04 21:51:07 +0200

    update

--- 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/06.Blog/app.js
+++ 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/06.Blog/app.js
@@ -1,5 +1,45 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoadPosts').addEventListener('click', getAllPosts);
+    document.getElementById('btnViewPost').addEventListener('click', displayPost);
+
 }
 
-attachEvents();
+function displayPost() {
+
+}
+
+async function getAllPosts() {
+    const postsURL = 'http://localhost:3030/jsonstore/blog/posts';
+
+    const postsRes = await fetch(postsURL)
+    const postsData = await postsRes.json();
+    const selectElement = document.getElementById('posts');
+    selectElement.replaceChildren();
+
+    Object.values(postsData).forEach(p => {
+        const optionElement = document.createElement('option');
+        optionElement.textContent = p.title;
+        optionElement.value = p.id;
+        selectElement.appendChild(optionElement);
+    });
+}
+
+async function getPostByID(postID) {
+    const postURL = 'http://localhost:3030/jsonstore/blog/posts/' + postID;
+
+    const postRes = await fetch(postURL)
+    const postData = await postRes.json();
+
+    return postData;
+}
+
+async function getCommentsByPostID(postID) {
+    const commentsURL = 'http://localhost:3030/jsonstore/blog/comments';
+
+    const commentsRes = await fetch(commentsURL);
+    const commentsData = await commentsRes.json();
+
+    const comments = Object.values(commentsData).filter(comment => comment.postId == postID);
+
+    return comments;
+}

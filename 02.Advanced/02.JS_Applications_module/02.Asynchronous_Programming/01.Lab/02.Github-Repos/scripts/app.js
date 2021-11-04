65bd74efb56709231031788f2b3adc954f41da85
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-11-04 13:38:40 +0200

    update

--- 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/01.Lab/02.Github-Repos/scripts/app.js
+++ 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/01.Lab/02.Github-Repos/scripts/app.js
@@ -1,30 +1,52 @@
-function loadRepos() {
+async function loadRepos() {
 	const username = document.getElementById('username').value;
 	const url = `https://api.github.com/users/${username}/repos`;
 	const list = document.getElementById('repos');
-	
-	fetch(url)
-		.then(res => {
-			if (res.ok == false) {
-				throw new Error(`${res.status} ${res.statusText}`);
-			}
-			return res.json();
-		})
-		.then(handleResponse)
-		.catch(handleError);
-	function handleResponse(data) {
+	try {
+		const response = await fetch(url);
 		list.innerHTML = '';
-
+		if (response.ok == false) {
+			throw new Error(`${response.status} ${response.statusText}`);
+		}
+		const data = await response.json();
 		for (let repo of data) {
 			const liElement = document.createElement('li');
-			liElement.innerHTML = `
-			<a href="{repo.html_url}">${repo.full_name}</a>
-			`;
+			liElement.innerHTML = `<a href="{repo.html_url}">${repo.full_name}</a>`;
 			list.appendChild(liElement);
 		}
-	}
-	function handleError(error) {
-		list.innerHTML = '';
+	} catch (error) {
 		list.textContent = `${error.message}`;
 	}
 }
+
+//** BEFORE ASYNC*/
+// function loadRepos() {
+// 	const username = document.getElementById('username').value;
+// 	const url = `https://api.github.com/users/${username}/repos`;
+// 	const list = document.getElementById('repos');
+	
+// 	fetch(url)
+// 		.then(res => {
+// 			if (res.ok == false) {
+// 				throw new Error(`${res.status} ${res.statusText}`);
+// 			}
+// 			return res.json();
+// 		})
+// 		.then(handleResponse)
+// 		.catch(handleError);
+// 	function handleResponse(data) {
+// 		list.innerHTML = '';
+
+// 		for (let repo of data) {
+// 			const liElement = document.createElement('li');
+// 			liElement.innerHTML = `
+// 			<a href="{repo.html_url}">${repo.full_name}</a>
+// 			`;
+// 			list.appendChild(liElement);
+// 		}
+// 	}
+// 	function handleError(error) {
+// 		list.innerHTML = '';
+// 		list.textContent = `${error.message}`;
+// 	}
+// }

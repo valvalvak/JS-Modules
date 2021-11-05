4895ca85773987db10927ed54b87840d6e0d7fd4
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-11-05 08:44:52 +0200

    update

--- 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/02.Bus-Schedule/app.js
+++ 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/02.Bus-Schedule/app.js
@@ -1,24 +1,26 @@
-async function solve() {
+function solve() {
     const label = document.querySelector('#info span');
     const departBtn = document.getElementById('depart');
     const arriveBtn = document.getElementById('arrive');
     let stop = {
         next: 'depot'
-    }
-    
+    };    
 
-    function depart() {
-        departBtn.disabled = true
-        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
-        const response = await fetch(url);
-        stop = await response.json();        
-        label.textContent = `Next stop ${data.name}`;
-        arriveBtn.disabled = false;
+    async function depart() {
+        departBtn.disabled = true;
         
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+
+        const res = await fetch(url);        
+        stop = await res.json();
+        
+        label.textContent = `Next stop ${stop.name}`;
+        
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        label.textContent = `Arriving at ${data.name}`;
+        label.textContent = `Arrived at ${stop.name}`;
         departBtn.disabled = false;
         arriveBtn.disabled = true;
     }

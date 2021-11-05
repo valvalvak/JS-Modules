13c2618f8cc7984189a5eb6550a892d54c3db06c
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-11-05 12:57:52 +0200

    update

--- 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/03.Forecaster/app.js
+++ 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/03.Forecaster/app.js
@@ -1,10 +1,39 @@
 function attachEvents() {
-    console.log("TODO...");
+    document.getElementById('submit').addEventListener('click', getForecast);
 }
 
 attachEvents();
-
-async function getLocationCode(name) {
-    const url = ''
-    
+async function getForecast(name) {
+    const cityCode = await getCityCode(name);
+    const [currentCity, nextCity]Promise.all([
+        getCurrentCityData(cityCode),
+        getNextCityData(cityCode)
+    ])
+} 
+async function getCityCode(name) {
+    const url = 'http://localhost:3030/jsonstore/forecaster/locations'
+    try {
+        const response = await fetch(url);
+        if (response.ok == false) {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        const location = data.find(l => l.name == name);
+        return location.code
+    } catch (error) {
+        console.log("Error")
+        // ....textContent = `${error.message}`;
+    }
+}
+async function getCurrentCityData(code) {
+    const url = 'http://localhost:3030/jsonstore/forecaster/today/' + code
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
+}
+async function getNextCityData(code) {
+    const url = 'http://localhost:3030/jsonstore/forecaster/upcoming/' + code
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
 }

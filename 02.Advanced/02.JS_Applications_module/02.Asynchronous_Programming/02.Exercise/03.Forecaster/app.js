6844ff43c3e64a7e9d3120133e0f182e07d598e1
Valentin Vakrilov <vvakrilov@gmail.com>
2021-11-14 15:06:18 +0200

    update

--- 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/03.Forecaster/app.js
+++ 02.Advanced/02.JS_Applications_module/02.Asynchronous_Programming/02.Exercise/03.Forecaster/app.js
@@ -1,16 +1,25 @@
-function attachEvents() {
-    document.getElementById('submit').addEventListener('click', getForecast);
+async function attachEvents() {
+    const elements = {
+        userInput: document.getElementById('location').value.trim(),
+        forecast: document.getElementById(forecast),
+        currentWeather: this.forecast.children[0],
+        currentWeather: this.forecast.children[1],
+    }
+    document.getElementById('submit').onclick = await getForecast(elements.userInput)
+    elements.userInput.value = ''
 }
-
 attachEvents();
+
 async function getForecast(name) {
     const cityCode = await getCityCode(name);
-    const [currentForecast, upcomingForecast] = Promise.all([
-        getCurrentCityForecastData(cityCode),
-        getNextCityForecastData(cityCode)
+    console.log(cityCode)
+    const [currentForecast, upcomingForecast] = await Promise.all([
+        getCurrent(cityCode),
+        getUpcoming(cityCode)
     ])
-    return {currentForecast, upcomingForecast}
-} 
+    console.log(currentForecast)
+    console.log(upcomingForecast)
+}
 async function getCityCode(name) {
     const url = 'http://localhost:3030/jsonstore/forecaster/locations'
     try {
@@ -26,13 +35,13 @@ async function getCityCode(name) {
         // ....textContent = `${error.message}`;
     }
 }
-async function getCurrentCityForecastData(code) {
+async function getCurrent(code) {
     const url = 'http://localhost:3030/jsonstore/forecaster/today/' + code
     const response = await fetch(url);
     const data = await response.json();
     return data
 }
-async function getNextCityForecastData(code) {
+async function getUpcoming(code) {
     const url = 'http://localhost:3030/jsonstore/forecaster/upcoming/' + code
     const response = await fetch(url);
     const data = await response.json();
---
+++ 02.Advanced/99.MyTests/useful4.js
@@ -0,0 +1,29 @@
+let current = {
+    "forecast": {
+      "condition": "Rain",
+      "high": "8",
+      "low": "2"
+    },
+    "name": "London, UK"
+}
+let upcoming = {
+    "forecast": [
+      {
+        "condition": "Rain",
+        "high": "8",
+        "low": "6"
+      },
+      {
+        "condition": "Rain",
+        "high": "11",
+        "low": "3"
+      },
+      {
+        "condition": "Rain",
+        "high": "8",
+        "low": "5"
+      }
+    ],
+    "name": "London"
+}
+

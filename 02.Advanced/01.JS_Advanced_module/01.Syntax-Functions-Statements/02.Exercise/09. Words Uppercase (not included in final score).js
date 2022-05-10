d4399970fd73fad7c2bb88a8f2e4b91b5ae5bb55
Valentin V.Vakrilov <vvakrilov@gmail.com>
2022-05-10 09:45:34 +0300

    updated function

--- 02.Advanced/01.JS_Advanced_module/01.Syntax-Functions-Statements/02.Exercise/09. Words Uppercase (not included in final score).js
+++ 02.Advanced/01.JS_Advanced_module/01.Syntax-Functions-Statements/02.Exercise/09. Words Uppercase (not included in final score).js
@@ -1,7 +1,33 @@
 function solve(givenString){
-    let re = /[A-Za-z]+/gm;
+    let re = /\w+/gm;
     strArr = [...givenString.matchAll(re)];
     console.log(strArr.join(", ").toUpperCase());
 }
+
 solve("Hi, how are you?");
 solve('hello');
+
+
+// function wordsUppercase([input]) {
+//     function extractWords() {
+//         return input.toUpperCase()
+//     return extractWords().join(', ');
+// }
+// }
+
+// function wordsUppercase([input]) {
+//     return input.toUpperCase()
+// }
+
+function wordsUppercase(text) {
+    let result = text
+            .toUpperCase()
+            .split(/\W+/gm)
+            .filter(x => x !== '')
+            .join(', '); 
+    return console.log(result)
+}
+
+wordsUppercase('Hi, how are you?');
+wordsUppercase('hello');
+

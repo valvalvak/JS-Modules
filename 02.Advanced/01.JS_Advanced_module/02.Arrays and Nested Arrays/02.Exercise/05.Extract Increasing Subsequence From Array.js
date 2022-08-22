37ecd7d7511cf7effc7e9b699d4e7577ebc4b48a
Valentin V. Vakrilov <vvakrilov@outlook.com>
2022-08-22 23:40:41 +0300

    solved with reduce func

--- 02.Advanced/01.JS_Advanced_module/02.Arrays and Nested Arrays/02.Exercise/05.Extract Increasing Subsequence From Array.js
+++ 02.Advanced/01.JS_Advanced_module/02.Arrays and Nested Arrays/02.Exercise/05.Extract Increasing Subsequence From Array.js
@@ -1,59 +1,64 @@
 // function solve(array){
-//     let result = [];
-//     result.push(array[0]);
+//     let res = [];
+//     res.push(array[0]);
 //     array.forEach(element => {
-//         if (element > Math.max(...result)){
-//             result.push(element);
-//         }        
+//         if (element > Math.max(.. res)){
+//             res.push(element);
+//         }
 //     });
-//     return result;
+//     return res;
 // }
 
 // function solve(array){
-//     let result = [];
+//     let res = [];
 //     let biggestNum = Number.MIN_SAFE_INTEGER
 //     array.forEach(element => {
 //         if (element > biggestNum){
-//             result.push(element);
+//             res.push(element);
 //             biggestNum = element;
-//         }        
+//         }
 //     });
-//     return result;
+//     return res;
 // }
 
-function solve(array) {
-    let biggestNum = Number.MIN_SAFE_INTEGER;
-    const result = array.filter((element) => {
-        if(element > biggestNum) {
-            biggestNum = element;
-            return true
-        }
-        return false
-    });
-    return result;
-}
+// function solve(array) {
+//   const res = [];
+//   for (const value of array) {
+//     if (
+//       value / 1 === value &&
+//       (res.length == 0 || res[res.length - 1] < value)
+//     ) {
+//       res.push(value);
+//     }
+//   }
+//   return res;
+// }
+ function solve(arr){
+  const res = [];
+  let biggestNum = Number.MIN_SAFE_INTEGER;
+  arr.reduce((accumulated, current) => {
+    if (current >= biggestNum){
+      biggestNum = current;
+      accumulated.push(biggestNum);
+    }
+    return accumulated;
+  }, res)
+  return res;
+ }
+// function solve(givenArray){
+//     let res = [givenArray[0]];
+//     for (const arg of givenArray){
+//         if (arg < res[length res-1]){
+//             continue;}
+//         else{
+//             res.append(arg);
+//             }
+//     }
+//     return res;
+// }
 
-// console.log(solve([
-//     1,
-//     3,
-//     8,
-//     4,
-//     10,
-//     12,
-//     3,
-//     2,
-//     24]));
+console.log(solve([NaN, "5", "6", 1, 1, 3, 8, 4, 10, 12, 3, 2, 24, NaN]));
 
-// console.log(solve([
-//     1,
-//     2,
-//     3,
-//     4]));
+console.log(solve([1, 2, 3, 4]));
 
-// console.log(solve([
-//     20, 
-//     3, 
-//     2, 
-//     15,
-//     6, 
-//     1]));
+console.log(solve([20, 3, 2, 15, 6, 1]));

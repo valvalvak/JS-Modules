bbd1c1aa64d267aa21b3fdec427c47914b3c49f6
Valentin Vakrilov <vvakrilov@gmail.com>
2022-11-26 12:55:35 +0200

    learning code updates

--- 02.Advanced/01.JS_Advanced_module/02.Arrays and Nested Arrays/02.Exercise/06.List Of Names.js
+++ 02.Advanced/01.JS_Advanced_module/02.Arrays and Nested Arrays/02.Exercise/06.List Of Names.js
@@ -1,8 +1,18 @@
-function solve(array) {
-    let result = [];
-    array.sort();
-    for(let i = 0;i < array.length;i++) {
-        console.log(`${i + 1}.${array[i]}`);
-    }
-}
+// function solve(array) {
+//     let result = [];
+//     array.sort();
+//     for(let i = 0;i < array.length;i++) {
+//         console.log(`${i + 1}.${array[i]}`);
+//     }
+// }
 // solve(["John", "Bob", "Christina", "Ema"]);
+function solve(array) {
+    array.sort(
+        (a, b) => a.localeCompare(b)
+        );
+    array.forEach(e => {
+        console.log(`${array.indexOf(e) + 1}.${e}`);
+        
+    });
+}       
+solve(["John", "Bob", "Christina", "Ema"]);
--- 02.Advanced/01.JS_Advanced_module/02.Arrays and Nested Arrays/02.Exercise/09.Magic Matrices.js
+++ 02.Advanced/01.JS_Advanced_module/02.Arrays and Nested Arrays/02.Exercise/09.Magic Matrices.js
@@ -1,22 +1,48 @@
+// function solve(array) {
+//     function sumRow(rowArray) {
+//         return rowArray.reduce((a,b) => a + b);
+//     }
+//     let sum = sumRow(array[0]);
+//     for(let row of array) {
+//         if(sum == sumRow(row)) {
+//             continue;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true
+// }
+
 function solve(array) {
-    function sumRow(rowArray) {
-        return rowArray.reduce((a,b) => a + b);
+    function sumArr(arr){
+        return arr.reduce((a, b) => a + b);
     }
-    let sum = sumRow(array[0]);
-    for(let row of array) {
-        if(sum == sumRow(row)) {
-            continue;
-        } else {
-            return false;
+    let sumsArr = [];
+    let currentRowSum = 0;
+    let currentColSum = 0;
+
+    for (let row = 0; row < array.length; row++) {
+        let currentRow = [];
+        currentRow = array[row]
+        currentRowSum =sumArr(currentRow); 
+        let currentColumnArray = [];
+
+        for (let col = 0; col < currentRow.length; col++) {
+            currentColumnArray.push(array[col][row]);
         }
-    }
-    return true
+    currentColSum = sumArr(currentColumnArray);
+    sumsArr.push(currentRowSum);
+  }
+  return sumsArr.every((value, i, arr) => value === arr[0]);
 }
-// console.log(solve([
+
+// console.log(
+//   solve([
 //     [4, 5, 6],
 //     [6, 5, 4],
-//     [5, 5, 5]
-// ]));
+//     [5, 5, 5],
+//   ])
+// );
 
 // console.log(solve([
 //     [11, 32, 45],

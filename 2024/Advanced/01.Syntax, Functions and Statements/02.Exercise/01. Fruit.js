953f91272672ec60d7afb9dce8c6e9a1474fad23
Tutzi <vvakrilov@gmail.com>
2024-05-19 12:16:23 +0300

    Syntax, Functions and Statements - Exercise

--- 2024/Advanced/01.Syntax, Functions and Statements/02.Exercise/01. Fruit.js
+++ 2024/Advanced/01.Syntax, Functions and Statements/02.Exercise/01. Fruit.js
@@ -1,6 +1,6 @@
 function solve(fruitType, weightInGrams, pricePerKg){
-    let weight = Math.round(Number(weightInGrams) / 100);  
+    let weight = Number(weightInGrams) / 1000;  
     let money = weight * Number(pricePerKg) ; 
-    console.log(`I need ${money} to buy ${weight} kilograms ${fruitType}.`)
+    console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruitType}.`)
 }
 solve('orange', 2500, 1.80)

4164a2182c462d9dfb4d0da1ce687d5fa97d22a8
Tutzi <vvakrilov@gmail.com>
2024-05-31 20:02:50 +0300

    JS Advanced module 2024 exercise

--- 2024/Advanced/01.Syntax, Functions and Statements/02.Exercise/07.CookingByNumbers.js
+++ 2024/Advanced/01.Syntax, Functions and Statements/02.Exercise/07.CookingByNumbers.js
@@ -1,4 +1,21 @@
-function solve(){
+function solve() {
+    const fn = {
+        'chop': (a) => a / 2,
+        'dice': (a) => Math.sqrt(a),
+        'spice': (a) => a + 1,
+        'bake': (a) => a * 3,
+        'fillet': (a) => a * .8,
+    }
+    let value = Number(arguments[0]);
+    for (let i = 1; i < arguments.length; i++) {
+        const operand = arguments[i];
+        let result = fn[operand](value)
+        console.log(result)
+        value = result
+    }
+
 
 }
-solve('32', 'chop', 'chop', 'chop', 'chop', 'chop')
+
+// solve('32', 'chop', 'chop', 'chop', 'chop', 'chop')
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet')

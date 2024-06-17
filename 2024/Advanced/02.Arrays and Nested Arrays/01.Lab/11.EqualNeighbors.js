50e0bb688d8088a5c7ea3f385efe4e07378864f7
Tutzi <vvakrilov@gmail.com>
2024-06-17 11:59:12 +0300

    Arrays and Nested Arrays - Exercise

--- 2024/Advanced/02.Arrays and Nested Arrays/01.Lab/11.EqualNeighbors.js
+++ 2024/Advanced/02.Arrays and Nested Arrays/01.Lab/11.EqualNeighbors.js
@@ -1,19 +1,57 @@
-function solve(matrix) {
-    const deltas = [
-        [-1][-1],
-        
-    ]
+function solve(matrix = []) {
+    let pairs = 0;
+    function getNewIndex(i) {
+        if (i < 0) {
+            i = 0;
+        }
+        return i
+    }
+    function checkPairs(currentEl, elements) {
+        for (el of elements) {
+            if (el === currentEl) {
+                pairs += 1
+            }
+        }
+    }
+    for (let x = 0; x < matrix.length; x++) {
+        for (let y = 0; y < matrix[x].length; y++) {
 
-};
-solve([
-    ['2', '3', '4', '7', '0'],
-    ['4', '0', '5', '3', '4'],
-    ['2', '3', '5', '4', '2'],
-    ['9', '8', '7', '5', '4'],
-]);
+            let currentEl = matrix[x][y];
+            let leftIndex = getNewIndex(y - 1);
+            let upperIndex = getNewIndex(x - 1);
 
-solve([
-    ['test', 'yes', 'yo', 'ho'],
-    ['well', 'done', 'yo', '6'],
-    ['not', 'done', 'yet', '5'],
-]);
+            let leftEl = ''
+            let upperEl = ''
+
+            if (upperIndex == x && leftIndex == y) {
+                continue
+            }
+            if (upperIndex != x) {
+                upperEl = matrix[upperIndex][y]
+            }
+            if (leftIndex != y) {
+                leftEl = matrix[x][leftIndex]
+            }
+            checkPairs(currentEl, [leftEl, upperEl])
+        }
+    }
+    return pairs
+}
+
+
+console.log(solve([
+    [2, 2, 5, 7, 4,],
+    [2, 5, 5, 4, 2,],
+    [4, 0, 5, 3, 4,],
+]))
+// console.log(solve([
+//     ['2', '3', '4', '7', '0'],
+//     ['4', '0', '5', '3', '4'],
+//     ['2', '3', '5', '4', '2'],
+//     ['9', '8', '7', '5', '4'],
+// ]))
+// console.log(solve([
+//     ['test', 'yes', 'yo', 'ho'],
+//     ['well', 'done', 'yo', '6'],
+//     ['not', 'done', 'yet', '5'],
+// ]))

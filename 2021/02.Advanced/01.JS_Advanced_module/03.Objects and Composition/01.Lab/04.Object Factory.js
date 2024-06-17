f7a60e597fd545d26d12346aa2d3432643c79e7f
Tutzi <vvakrilov@gmail.com>
2024-06-18 02:16:40 +0300

    Objects & Composition - Lab

--- 2021/02.Advanced/01.JS_Advanced_module/03.Objects and Composition/01.Lab/04.Object Factory.js
+++ 2021/02.Advanced/01.JS_Advanced_module/03.Objects and Composition/01.Lab/04.Object Factory.js
@@ -20,23 +20,23 @@ const library = {
         console.log(`${this.name} is playing '${track}' by ${artist}`);
     },
 };
-// const orders = [
-//     {
-//         template: { name: 'ACME Printer' },
-//         parts: ['print']
-//     },
-//     {
-//         template: { name: 'Initech Scanner' },
-//         parts: ['scan']
-//     },
-//     {
-//         template: { name: 'ComTron Copier' },
-//         parts: ['scan', 'print']
-//     },
-//     {
-//         template: { name: 'BoomBox Stereo' },
-//         parts: ['play']
-//     }
-// ];
-// const products = factory(library, orders);
-// console.log(products);
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
+const products = factory(library, orders);
+console.log(products);

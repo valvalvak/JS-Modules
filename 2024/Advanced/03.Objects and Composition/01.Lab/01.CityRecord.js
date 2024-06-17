93c9badd44e5a3602582900528cec78c520b112a
Tutzi <vvakrilov@gmail.com>
2024-06-17 19:51:04 +0300

    Objects & Composition - Lab

---
+++ 2024/Advanced/03.Objects and Composition/01.Lab/01.CityRecord.js
@@ -0,0 +1,11 @@
+function solve(name=String, population=Number, treasury=Number){
+    return{
+        name,
+        population,
+        treasury
+    }
+}
+console.log(solve('Tortuga',
+7000,
+15000
+))

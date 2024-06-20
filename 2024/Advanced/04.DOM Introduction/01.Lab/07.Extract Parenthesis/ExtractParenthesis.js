7a0a2f34d8b9876ade8d58b1a2c3bbb34d8b37fc
Tutzi <vvakrilov@gmail.com>
2024-06-20 10:45:04 +0300

    DOM Introduction - Lab

--- 2024/Advanced/04.DOM Introduction/01.Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 2024/Advanced/04.DOM Introduction/01.Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,21 @@
 function extract(content) {
+    // let string = document.getElementById(content).textContent;
+    // let patternn = /\(([^)]+)\)/g;
+    // let result = [];
 
+    // let match = pattern.exec(string);
+
+    // while(match){
+
+    //     result.push(match[1]);
+    //     match = pattern.exec(string)
+    // }
+    // return result.join('; ')
+    const element = document.getElementById(content);
+    const pattern = /\(([^\(\)]+)\)/g;
+    const matches = element.textContent.matchAll(pattern)
+    // console.log(Array.from(matches))
+    const result = Array.from(matches).map(match => match[1]).join('; ')
+    // console.log(result)
+    return result
 }

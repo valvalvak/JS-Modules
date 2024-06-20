c2e1bf11998c4f6847dd93d9a03647ef1f5c8e6a
Tutzi <vvakrilov@gmail.com>
2024-06-20 09:41:46 +0300

    DOM Introduction - Lab

--- 2024/Advanced/04.DOM Introduction/01.Lab/06.Sum Table/SumTable.js
+++ 2024/Advanced/04.DOM Introduction/01.Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+    const table = document.querySelectorAll("table tr")
+    let sum = 0
+    for (let i =1; i< table.length; i++){
+        let columns = table[i].children;
+        let price = columns[columns.length - 1].textContent;
+        sum += Number(price)
+    }
+    document.getElementById("sum").textContent = sum;
 }

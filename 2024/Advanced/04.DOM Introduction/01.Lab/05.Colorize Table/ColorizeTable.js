971ae2e1d1fc214b5685f3f5cb11131ec702ad14
Tutzi <vvakrilov@gmail.com>
2024-06-20 09:22:40 +0300

    DOM Introduction - Lab

--- 2024/Advanced/04.DOM Introduction/01.Lab/05.Colorize Table/ColorizeTable.js
+++ 2024/Advanced/04.DOM Introduction/01.Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,26 @@
 function colorize() {
-    // TODO
+    // SOLUTION CONDITIONS:
+    
+    // Write a JS function that changes the color of all even rows when the user clicks a button. 
+    // Apply the color "Teal" to the target rows.
+    // There will be no input/output, your program should instead modify the DOM of the given HTML document.
+    
+    // LAB SOLUTION
+    
+    // let rows = document.querySelectorAll("table tr");
+    // let index = 0;
+    // for (let row of rows) {
+    //     index++;
+    //     if (index % 2 == 0)
+    //         row.style.background = "teal";
+    // }
+    
+    // MY TEST SOLUTION
+    let rows = document.querySelectorAll("table tr");
+    for (let i = 0; i < rows.length; i++) {
+        if (i % 2 != 0) {
+            rows[i].style.background = "teal"
+        }
+    }
+
 }
